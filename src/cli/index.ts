56c6080c479e3955b890e715b11758b401b1a23a
dyoshikawa <dyoshikawa1993@gmail.com>
2026-03-30 09:09:55 +0000

    chore: bump version to 7.24.0

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -17,7 +17,7 @@ import { mcpCommand } from "./commands/mcp.js";
 import { updateCommand, UpdateCommandOptions } from "./commands/update.js";
 import { wrapCommand as _wrapCommand } from "./wrap-command.js";
 
-const getVersion = () => "7.23.0";
+const getVersion = () => "7.24.0";
 
 function wrapCommand(
   name: string,
