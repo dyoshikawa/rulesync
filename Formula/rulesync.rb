7391864606715f9b343cbab1c0f5ed62707a6037
dyoshikawa <dyoshikawa1993@gmail.com>
2026-07-14 06:33:58 +0000

    chore: update Homebrew formula to v9.7.0

--- Formula/rulesync.rb
+++ Formula/rulesync.rb
@@ -7,28 +7,28 @@
 class Rulesync < Formula
   desc "Unified AI rules management CLI that generates config files for AI dev tools"
   homepage "https://github.com/dyoshikawa/rulesync"
-  version "9.6.3"
+  version "9.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/dyoshikawa/rulesync/releases/download/v#{version}/rulesync-darwin-arm64"
-      sha256 "90844bbf409016b3641578ff4a8508a1d748d558933c66c9ff74471e1a250492"
+      sha256 "2a74b72a5f4bf87a925088a7eca41ea420cfa0226896dc9d2178ba6bfe735326"
     end
     on_intel do
       url "https://github.com/dyoshikawa/rulesync/releases/download/v#{version}/rulesync-darwin-x64"
-      sha256 "1ac8cf074608a02d5a16d4a9f331b560757db5ed1821229622d10fcadb9f46d4"
+      sha256 "279b57d476def520bc9aee01725d1fe1fdf13998ccdea8dd5b13151d204d5f01"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dyoshikawa/rulesync/releases/download/v#{version}/rulesync-linux-arm64"
-      sha256 "3bf6af02261ee1ac6fa5533a3db2a76554ba93819313533cf419182bc9dc202f"
+      sha256 "c26a1518b563f96a8549406b4c26a7ed46c8360786d732e47eecf9bcf4fff704"
     end
     on_intel do
       url "https://github.com/dyoshikawa/rulesync/releases/download/v#{version}/rulesync-linux-x64"
-      sha256 "613a098b1d9f993151ade442edd7383d1f30972600dda5cdd8f7343bec1c5404"
+      sha256 "b5241cef579d32a4ac40a67d417888f7052f911095d587346d8349740f2fa209"
     end
   end
 
